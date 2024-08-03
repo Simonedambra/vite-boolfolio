0e427f1478aac5c9c1beeb4c15920205baeed335
Simonedambra <simonedambra130102@gmail.com>
2024-08-03 18:58:37 +0200

    upd

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios, { Axios } from 'axios';
+
+export default{
+  title:'projects',
+  url:'http://127.0.0.1:8000/api/project',
+  data(){
+return {
+  
+}
+  },
+  methods:{
+getprojects(){
+  const result= axios.get(url).then(Response =>{
+    console.log(Response)
+  })
+}
+
+  },
+  created(){
+    this.getprojects();
+  }
+
+};
+</script> 
+<template>
+  <h1>Vue Vite Template</h1>
+  <p>Template di partenza per progetti Vite Vue</p>
+</template>
+<!-- <style></style> -->
