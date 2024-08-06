12f38e02cdc8bb4dae730d837dcbdebecac411ec
Simonedambra <simonedambra130102@gmail.com>
2024-08-06 09:02:56 +0200

    upd file store project card

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <script>
 import axios, { Axios } from 'axios';
-
+import projects from './components/project.vue';
+import { store } from './store';
 export default{
-  title:'projects',
-  url:'http://127.0.0.1:8000/api/project',
+  title:'project_app',
+  components: {
+    projects  // Registra il componente Project
+  },
   data(){
 return {
-  
+ 
+  store,
+  projects:[]
 }
   },
   methods:{
-getprojects(){
-  const result= axios.get(url).then(Response =>{
-    console.log(Response)
-  })
-}
+getprojects() {
+            axios.get(this.store.apiUrl)
+                .then((response) => {
+                  console.log(response.data)
+                    this.store.projects= response.data
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+
 
   },
   created(){
     this.getprojects();
+    
   }
 
 };
 </script> 
 <template>
   <h1>Vue Vite Template</h1>
+  
+  <projects></projects>
   <p>Template di partenza per progetti Vite Vue</p>
 </template>
 <!-- <style></style> -->
